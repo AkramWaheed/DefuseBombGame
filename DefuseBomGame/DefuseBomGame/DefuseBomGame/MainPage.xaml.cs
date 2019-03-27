c1d54d36eaee61c8291bdb621ce826a3560be19d
Waheed <g00341970@gmit.ie>
2019-03-27 00:16:34 +0000

    Game intial layout

---
+++ DefuseBomGame/DefuseBomGame/DefuseBomGame/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DefuseBomGame
+{
+    public partial class MainPage : ContentPage
+    {
+        //initializing variables
+        static string bomb = new Random().Next(1, 10).ToString();
+        static int score = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //not returning anythiing we will use 
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            //Game Over
+            if (button.Text == bomb)
+            {
+                await DisplayAlert("Bomb Exploded !", "Game Over", "Retry");
+                bomb = new Random().Next(1, 10).ToString();
+                score = 0;
+            }//end of if statement
+            else
+            {
+                score += 1;
+                await DisplayAlert("Bomb Defused !", "Scores" + score, "Continue");
+                bomb = new Random().Next(1, 10).ToString();
+            }//end of else statement
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 # it's a college project ,using C# with the help of cross plateform
 # Project is given by Damien Costello
 # Galway Mayo Institute of Technology
+
+Initial Game layout is designed ….
